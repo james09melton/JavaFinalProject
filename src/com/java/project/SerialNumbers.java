933904e60445f8ac071574065ff12ed0e3f3cc11
James Melton <james09melton@gmail.com>
2018-12-17 15:20:53 -0600

    nk

--- src/com/java/project/SerialNumbers.java
+++ src/com/java/project/SerialNumbers.java
@@ -3,7 +3,7 @@ package com.java.project;
 public class SerialNumbers {
 	private static SerialNumbers instance; 
 	public enum productTypes{
-		LargeGadget, MediumGadget, SmallGadget, LargeWidge, MediumWidget, SmallWidget;
+		LargeGadget, MediumGadget, SmallGadget, LargeWidget, MediumWidget, SmallWidget;
 	}
 	
 	private String productPrefix;
@@ -17,32 +17,44 @@ public class SerialNumbers {
 	private SerialNumbers() { }
 	
 	public synchronized String getNextSerial(productTypes types) {
-	int count;
+	int count, next;
 	switch(types) {
 	case LargeGadget:
 		count = 5342;
 	productPrefix = "32LG";
-	int next = ++ count;
+	next = ++ count;
 	return productPrefix + next;
-	}
 	
-	public synchronized String getNextSerial2(productTypes types) {
-	int count;
-	switch(types) {
 	case MediumGadget:
 		count = 1853;
 	productPrefix = "23LG";
-	int next = ++ count;
+	next = ++ count;
 	return productPrefix + next;
-	}
 	
-	public synchronized String getNextSerial3(productTypes types) {
-	int count;
-	switch(types) {
 	case SmallGadget:
 		count = 4689;
 	productPrefix = "85LG";
-	int next = ++ count;
+	next = ++ count;
 	return productPrefix + next;
+	
+	case LargeWidget:
+		count = 3275;
+	productPrefix = "96LG";
+	next = ++ count;
+	return productPrefix + next;
+	
+	case MediumWidget:
+		count = 9618;
+	productPrefix = "75LG";
+	next = ++ count;
+	return productPrefix + next;
+	
+	case SmallWidget:
+		count = 8520;
+	productPrefix = "12LG";
+	next = ++ count;
+	return productPrefix + next;
+	default: return "no type defined";
+		}
 	}
 }
